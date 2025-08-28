d381796368f9d13374fbbb76f52f4f0f3509a83e
chenwenhao <chenwenhao>
2025-08-28 18:04:45 +0800

    update capture node

--- node-red-contrib-sscma/nodes/capture.js
+++ node-red-contrib-sscma/nodes/capture.js
@@ -5,47 +5,18 @@ module.exports = function (RED) {
         node.client = RED.nodes.getNode(config.client);
         node.config = {
             storage: config.storage,
-            interval: +config.interval * Math.pow(60, Number(config.timeUnit)),
+            slice: +config.interval * Math.pow(60, Number(config.timeUnit)),
+            duration: 0,
             enabled: config.startMode === "immediate",
             saveMode: "image",
         };
-        let intervalHandle = null;
-
-        function performCapture() {
-            if (node.client) {
-                node.client.request(node.id, "capture", true);
-            }
-        }
-
-        function startInterval() {
-            stopInterval();
-            const seconds = Number(node.config.interval) || 0;
-            if (seconds <= 0) {
-                return;
-            }
-            intervalHandle = setInterval(performCapture, seconds * 1000);
-            // Optional: immediately capture once on enable
-            performCapture();
-        }
-
-        function stopInterval() {
-            if (intervalHandle) {
-                clearInterval(intervalHandle);
-                intervalHandle = null;
-            }
-        }
 
         node.on("input", function (msg) {
             if (msg.hasOwnProperty("enabled")) {
                 node.client.request(node.id, "enabled", msg.enabled);
-                if (msg.enabled === true) {
-                    startInterval();
-                } else {
-                    stopInterval();
-                }
             }
             if (msg.payload === "capture") {
-                performCapture();
+                node.client.request(node.id, "capture", true);
             }
         });
 
@@ -57,17 +28,11 @@ module.exports = function (RED) {
             node.client.register(node);
         }
 
-        // Start interval if configured to start immediately
-        if (node.config.enabled) {
-            startInterval();
-        }
-
         node.on("close", function (removed, done) {
             if (node.client) {
                 node.client.deregister(node, done, removed);
                 node.client = null;
             }
-            stopInterval();
             done();
         });
     }
